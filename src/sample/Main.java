777a6f2b51f539e94dec1f65ae3e50daa31b0e93
Marcin Putra <marcin.putra@gmail.com>
2020-12-02 20:35:56 +0100

    showing image in javaFX window, using fileChooser

---
+++ src/sample/Main.java
@@ -0,0 +1,57 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.embed.swing.SwingFXUtils;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class Main extends Application {
+
+    ImageView myImageView = new ImageView();
+
+    Stage stage = new Stage();
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+
+        Button btnBrowse = new Button("browse");
+        btnBrowse.setOnAction(event -> {
+            FileChooser fileChooser = new FileChooser();
+
+            FileChooser.ExtensionFilter extFilterJPG = new FileChooser.ExtensionFilter("JPG files (*.jpg)", "*.JPG");
+            FileChooser.ExtensionFilter extFilterPNG = new FileChooser.ExtensionFilter("PNG files (*.png)", "*.PNG");
+            fileChooser.getExtensionFilters().addAll(extFilterJPG, extFilterPNG);
+
+            File file = fileChooser.showOpenDialog(null);
+
+            try {
+                BufferedImage bufferedImage = ImageIO.read(file);
+                Image image = SwingFXUtils.toFXImage(bufferedImage, null);
+                myImageView.setImage(image);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+
+        VBox root = new VBox();
+        root.getChildren().addAll(btnBrowse, myImageView);
+        stage.setTitle("viewing image");
+        stage.setScene(new Scene(root, 500, 600));
+        stage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
